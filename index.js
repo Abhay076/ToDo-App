d2eb38c575e1f484eec06b6326be505bc97ba001
Abhay 076 <yadavabhaycs@gmail.com>
2022-08-14 21:58:54 +0530

    some changes

--- index.js
+++ index.js
@@ -12,11 +12,25 @@ app.set('view engine', 'ejs');
 // Setting path for views
 app.set('views', './views');
 
-
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 // to use static files, present in assets directory
 app.use(express.static('assets'));
 
 // Setting express to listen to port 8000
+app.use(session({
+    name: 'codeial',
+    // TODO change the secret before deployment in production mode
+    store: MongoStore.create(
+        {
+            mongoUrl:'mongodb://localhost/task_list_db',
+            autoRemove: 'disabled'
+        },
+        function(err){
+            console.log(err || 'connect-mongoDB setup ok');
+        }
+    )
+ }));
 app.listen(port, function(err){
     if(err){
         console.log("Error Occurred while trying to run server on port : ", port);
