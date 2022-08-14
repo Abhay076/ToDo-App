b0850958bd16d259d696a7dd317e611060a24d1c
Abhay 076 <yadavabhaycs@gmail.com>
2022-08-14 22:32:56 +0530

    last one

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //this is entry point
 const express = require('express');
-const port = 8000;
+const port =  process.env.PORT||8000;
 
 const app = express();
 
@@ -11,26 +11,11 @@ app.use('/', require('./routes'));
 app.set('view engine', 'ejs');
 // Setting path for views
 app.set('views', './views');
-
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
 // to use static files, present in assets directory
 app.use(express.static('assets'));
 
 // Setting express to listen to port 8000
-app.use(session({
-    name: 'codeial',
-    // TODO change the secret before deployment in production mode
-    store: MongoStore.create(
-        {
-            mongoUrl:'mongodb://localhost/task_list_db',
-            autoRemove: 'disabled'
-        },
-        function(err){
-            console.log(err || 'connect-mongoDB setup ok');
-        }
-    )
- }));
+
 app.listen(port, function(err){
     if(err){
         console.log("Error Occurred while trying to run server on port : ", port);
